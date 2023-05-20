0126d37abba9498f1b45124ae125e0660105293d
Mert Ernaz <rnzappdroid@gmail.com>
2023-05-21 00:12:10 +0300

    string tersten

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structures
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kelime giriniz:");
+            string entry=Console.ReadLine();
+            
+            ReverseWrite(entry);
+
+
+
+
+        }
+
+        public static void ReverseWrite(string s){
+
+            for(int i=0;i<s.Length;i++){
+                Console.Write(s[s.Length-1-i]);
+            }
+
+        }
+    }
+}
